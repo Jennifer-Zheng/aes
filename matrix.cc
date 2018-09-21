2fc2e2ca3fe948c620c457f12cb8763e55e441f7
Jong-Min Kim <jongmink@cs.utexas.edu>
2018-09-21 16:33:50 -0500

    Implement Key Expansion

--- matrix.cc
+++ matrix.cc
@@ -4,10 +4,147 @@
 #include <bitset>
 using namespace std;
 
+vector <unsigned char> SubWord(vector <unsigned char> word) {
+    unsigned char lookup_table[16][16] =
+    {
+        0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
+        0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
+        0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15,
+        0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75,
+        0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84,
+        0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF,
+        0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8,
+        0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2,
+        0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73,
+        0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB,
+        0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79,
+        0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08,
+        0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A,
+        0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E,
+        0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF,
+        0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16
+    };
+
+    for(int i = 0; i < 4; ++i) {
+        unsigned char value = word[i];
+        int lookup_row = (value & 0xF0) >> 4;
+        int lookup_col = value & 0x0F;
+        word[i] = lookup_table[lookup_row][lookup_col];
+    }
+    return word;
+}
+
+vector <unsigned char> RotWord(vector <unsigned char> word) {
+    unsigned char temp = word[0];
+    word[0] = word[1];
+    word[1] = word[2];
+    word[2] = word[3];
+    word[3] = temp;
+    return word;
+}
+
+vector <unsigned char> XorWords(vector <unsigned char> v1, vector <unsigned char> v2) {
+    vector<unsigned char> return_vector(4,0);
+    for(int i = 0; i < 4; ++i) {
+        return_vector[i] = v1[i] ^ v2[i];
+    }
+    return return_vector;
+}
+
+vector < vector<unsigned char> > KeyExpansion(vector<unsigned char> key, int key_length) {
+    int n_k = key_length / 32;
+    int n_r = 0;
+    if(key_length == 128) {
+        n_r = 10;
+    } else if(key_length == 256) {
+        n_r = 14;
+    }
+
+    vector < vector<unsigned char> > key_schedule(4, vector<unsigned char> (4*(n_r+1), 0));
+    
+    unsigned char rcon[] = {0x00,0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80,0x1B,0x36};
+
+    // Add the original key into the key_schedule
+    for(int i = 0; i < n_k; ++i) {
+        key_schedule[0][i] = key[4*i];
+        key_schedule[1][i] = key[4*i+1];
+        key_schedule[2][i] = key[4*i+2];
+        key_schedule[3][i] = key[4*i+3];
+    }
+
+    // Create the expanded part of the key_schedule
+    vector<unsigned char> temp(4,0);
+    for(int i = n_k; i < 4*(n_r+1); ++i) {
+        temp[0] = key_schedule[0][i-1];
+        temp[1] = key_schedule[1][i-1];
+        temp[2] = key_schedule[2][i-1];
+        temp[3] = key_schedule[3][i-1];
+
+        if(i % n_k == 0) {
+            vector<unsigned char> rcon_vec(4, 0x00);
+            rcon_vec[0] = rcon[i/n_k];
+            temp = XorWords(SubWord(RotWord(temp)), rcon_vec);
+        } else if (n_k == 8 && ((i % n_k) == 4)) {
+            temp = SubWord(temp);
+        }
+
+        vector<unsigned char> past_column(4, 0x00);
+        past_column[0] = key_schedule[0][i - n_k];
+        past_column[1] = key_schedule[1][i - n_k];
+        past_column[2] = key_schedule[2][i - n_k];
+        past_column[3] = key_schedule[3][i - n_k];
+
+        temp = XorWords(past_column, temp);
+        key_schedule[0][i] = temp[0];
+        key_schedule[1][i] = temp[1];
+        key_schedule[2][i] = temp[2];
+        key_schedule[3][i] = temp[3];
+    }
+
+    return key_schedule;
+}
+    
 int main() 
 {
-    // Opens a file a binary
+    ifstream key_file;
+    int key_length = 128;
+    vector<unsigned char> key;
+
+    key_file.open("key", ios::binary);
+    if(key_file.is_open()) 
+    {
+        char byte;
+        int key_bytes = 0;;
+        
+        while(key_file.get(byte))
+        {
+            key.push_back((unsigned char)byte);
+            ++key_bytes;
+        }
+
+        if((8 * key_bytes) != key_length) {
+            cout << "Your key is not " << key_length << "bits long " << endl;
+            cout << "Your key is " << (8 * key_bytes) << "bits long " << endl;
+            return 0;
+        }
+    } else {
+        cout << "Error with opening the key file" << endl;
+        return 0;
+    }
+
+    int n_k = key_length / 32;
+    int n_r = 0;
+    if(key_length == 128) {
+        n_r = 10;
+    } else if(key_length == 256) {
+        n_r = 14;
+    }
+    vector < vector<unsigned char> > key_schedule = KeyExpansion(key,key_length);
+
+    
+    // Opens a file as binary
     ifstream file;
+    vector< vector < vector<unsigned char> > > matrix_list;
     file.open("test.txt", ios::binary);
     if(file.is_open()) 
     {
@@ -16,17 +153,12 @@ int main()
         int matrix_pos = 0;
         int matrix_index = -1;
         // List of 4x4 Matrices
-        vector< vector < vector<char> > > matrix_list;
+        
     	while(file.get(byte)) {
-            /*
-            bitset<8> bits(byte);
-            cout << byte << ", " << bits << endl;
-            //*/
-
             // Keeps track of the position in the matrix the next byte should be
             // If it is 0, it means we should start a new matrix
             if(matrix_pos == 0) {
-                vector< vector<char> > matrix(4, vector<char> (4, 0));
+                vector< vector<unsigned char> > matrix(4, vector<unsigned char> (4, 0));
                 matrix_list.push_back(matrix);
                 // Update the current matrix
                 ++matrix_index;
@@ -37,7 +169,7 @@ int main()
             int row = matrix_pos % 4;
             int col = matrix_pos / 4;
 
-            matrix_list[matrix_index][row][col] = byte;
+            matrix_list[matrix_index][row][col] = (unsigned char)byte;
             
             // Updates the next position in the matrix & the number of bytes collectd
             matrix_pos = (matrix_pos+1) % 16;
@@ -48,7 +180,7 @@ int main()
         if (rem == 0) {
             // If we recieved a factor of 16 bytes, we add a new padding matrix
             cout << "ANOTHER" << endl;
-            vector< vector<char> > matrix(4, vector<char> (4, 16));
+            vector< vector<unsigned char> > matrix(4, vector<unsigned char> (4, 16));
             matrix_list.push_back(matrix);
         } else {
             // Otherwise, we pad the rest of the current matrix
@@ -59,7 +191,7 @@ int main()
                 matrix_list[matrix_index][row][col] = (16-rem);
             }
         }
-        
+        /*
         // Prints Matrix
         for(int i = 0; i < matrix_list.size(); ++i) 
         {
@@ -86,6 +218,8 @@ int main()
         //*/
 
     	file.close();
+    } else {
+        cout << "Error with opening the input file" << endl;
     }
     
     return 0;
