a036ce997df6b28db8a79ae8b17d34d4537e11e2
Jennifer-Zheng <jenn.zheng@yahoo.com>
2018-09-27 17:13:01 -0500

    moved function declarations to header file

--- aes.cc
+++ aes.cc
@@ -3,21 +3,7 @@
 #include <string>
 #include <vector>
 #include <bitset>
-
-void PrintMatrix (std::vector<std::vector<std::vector<unsigned char>>> matrix_list);
-
-void AddRoundKey (std::vector<std::vector<std::vector<unsigned char>>> &matrix_list, std::vector<std::vector<unsigned char>> key_schedule, int round_num);
-void SubBytes (std::vector<std::vector<std::vector<unsigned char>>> &input_array);
-void ShiftRows (std::vector<std::vector<std::vector<unsigned char>>> &input_array);
-void MixColumns (std::vector<std::vector<std::vector<unsigned char>>> &input_array);
-void InverseMixColumns (std::vector<std::vector<std::vector<unsigned char>>> &input_array);
-void InverseSubBytes (std::vector<std::vector<std::vector<unsigned char>>> &input_array);
-void InverseShiftRows (std::vector<std::vector<std::vector<unsigned char>>> &input_array);
-
-std::vector<unsigned char> SubWord (std::vector<unsigned char> word);
-std::vector<unsigned char> RotWord (std::vector<unsigned char> word);
-std::vector<unsigned char> XorWords (std::vector<unsigned char> v1, std::vector<unsigned char> v2);
-std::vector<std::vector<unsigned char>> KeyExpansion (std::vector<unsigned char> key, int key_length);
+#include "aes.h"
 
 int main (int argc, char **argv) {
     int key_size = std::stoi(argv[2]);
